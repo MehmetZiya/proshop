ac674177a2d5179045cd0e16c757595cc5fcd9c5
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-31 00:26:39 +0200

    Fixed Node error

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -71,9 +71,9 @@ const Rating = ({ value, text, color }) => {
 Rating.defaultProps = {
   color: '#f8e825',
 }
-Rating.propTypes = {
+/* Rating.propTypes = {
   value: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   color: PropTypes.string,
-}
+} */
 export default Rating
