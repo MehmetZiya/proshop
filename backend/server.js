9d702829e203629f8f67adbd8862ec1d6070de12
Mehmet Ziya <mehmetziya82@gmail.com>
2021-10-31 16:25:53 +0100

    added authorization staff to backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 
 //Routes
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Middleware
 app.use(notFound)
